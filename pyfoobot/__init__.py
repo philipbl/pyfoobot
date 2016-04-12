94ad8d03c342d9802072990ed9407ae3270d9d77
Philip Lundrigan <philiplundrigan@gmail.com>
2016-04-12 11:56:01 -0600

    Restructure API
    Now methods don't take a device, instead a device is created which can
    then be queried for data.

--- pyfoobot/__init__.py
+++ pyfoobot/__init__.py
@@ -1,68 +1,69 @@
 from collections import namedtuple
 import requests
 
-FoobotDevice = namedtuple('FooBotDevice', ['userId', 'uuid', 'name', 'mac'])
+BASE_URL = 'https://api.Foobot.io/v2'
 
 
 class Foobot:
     def __init__(self, username, password):
-        self.base_url = 'https://api.Foobot.io/v2'
         self.username = username
         self.password = password
         self.session = requests.Session()
 
         self.token = self.login()
         if self.token is None:
-            raise Exception("User does not exist.")
+            raise ValueError("Provided username or password is not valid.")
 
         self.auth_header = {'Accept': 'application/json;charset=UTF-8',
                             'x-auth-token': self.token}
 
-    @staticmethod
-    def _get_uuid(uuid):
-        if isinstance(uuid, FoobotDevice):
-            return uuid.uuid
-        else:
-            return uuid
-
     def login(self):
-        url = '{base}/user/{user}/login/'.format(base=self.base_url,
+        url = '{base}/user/{user}/login/'.format(base=BASE_URL,
                                                  user=self.username)
         req = self.session.get(url, auth=(self.username, self.password))
         return req.headers['X-AUTH-TOKEN'] if req.text == "true" else None
 
     def devices(self):
-        url = '{base}/owner/{user}/device/'.format(base=self.base_url,
+        url = '{base}/owner/{user}/device/'.format(base=BASE_URL,
                                                    user=self.username)
         req = self.session.get(url, headers=self.auth_header)
-        return [FoobotDevice(**device) for device in req.json()]
+        return [FoobotDevice(self.token, **device) for device in req.json()]
 
-    def latest(self, uuid):
-        uuid = self._get_uuid(uuid)
+
+class FoobotDevice:
+    def __init__(self, token, userId, uuid, name, mac):
+        self.token = token
+        self.userId = userId
+        self.uuid = uuid
+        self.name = name
+        self.mac = mac
+        self.session = requests.Session()
+        self.auth_header = {'Accept': 'application/json;charset=UTF-8',
+                            'x-auth-token': self.token}
+
+    def latest(self):
         url = '{base}/device/{uuid}/datapoint/{period}/last/{sampling}/'
-        url = url.format(base=self.base_url,
-                         uuid=uuid,
+        url = url.format(base=BASE_URL,
+                         uuid=self.uuid,
                          period=0,
                          sampling=0)
         req = self.session.get(url, headers=self.auth_header)
         return req.json()
 
-    def data_period(self, uuid, period, sampling):
-        uuid = self._get_uuid(uuid)
+    def data_period(self, period, sampling):
         url = '{base}/device/{uuid}/datapoint/{period}/last/{sampling}/'
-        url = url.format(base=self.base_url,
-                         uuid=uuid,
+        url = url.format(base=BASE_URL,
+                         uuid=self.uuid,
                          period=period,
                          sampling=sampling)
 
         req = self.session.get(url, headers=self.auth_header)
         return req.json()
 
-    def data_range(self, uuid, start, end, sampling):
-        uuid = self._get_uuid(uuid)
+    def data_range(self, start, end, sampling):
         url = '{base}/device/{uuid}/datapoint/{start}/{end}/{sampling}/'
-        url = url.format(base=self.base_url,
-                         uuid=uuid,
+        url = url.format(base=BASE_URL,
+                         uuid=self.uuid,
                          start=start,
                          end=end,
                          sampling=sampling)
